32ddf7d88925aed42cdaad8d27a1c995eb8fcb4d
sangyong-99 <tkddyd1126@gmail.com>
2022-06-30 13:11:28 +0900

    수정

---
+++ src/StringUse.java
@@ -0,0 +1,13 @@
+public class StringUse {
+    public static void main(String[] args) {
+        String str1 = new String("자바프로그래밍");
+        String str2= new String("자바프로그래밍");
+        String str3 = "자바프로그래밍";
+        String str4 = "자바프로그래밍";
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+    }
+}
